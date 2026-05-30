497599e882ff95db8e68f5d255c7cf8f7bf72623
Alim Sourav <sourav.0.alim@gmail.com>
2026-05-30 13:36:24 +0600

    runtime: add iosX64 target; fix resultOf null-window (SideEffect); delete ResultStore.kt; deprecate Animations legacy aliases

--- navease-runtime/build.gradle.kts
+++ navease-runtime/build.gradle.kts
@@ -30,7 +30,8 @@ kotlin {
 
     listOf(
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
+        iosX64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "navease-runtimeKit"
